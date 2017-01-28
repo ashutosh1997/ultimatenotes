fc148a1f6821e6be59a5e199d1aeb6e5baf2cab0
ASHUTOSH <kumar.ashutosh920@gmail.com>
2017-01-28 07:20:20 +0530

    Done

---
+++ js/home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ASHUTOSH on 28-Jan-17.
+ */
+$(document).on('click', function(event){
+    var $clickedOn = $(event.target),
+        $collapsableItems = $('.collapse'),
+        isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
+        isLink = ($clickedOn.closest('a').length == 1),
+        isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
+
+    if( (!isToggleButton && isLink) || isOutsideNavbar ) {
+        $collapsableItems.each(function(){
+            $(this).collapse('hide');
+        });
+    }
+});
--- js/sem.js
+++ js/sem.js
@@ -1,7 +1,34 @@
 
+$(document).on('click', function(event){
+    var $clickedOn = $(event.target),
+        $collapsableItems = $('.collapse'),
+        isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
+        isLink = ($clickedOn.closest('a').length == 1),
+        isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
+
+    if( (!isToggleButton && isLink) || isOutsideNavbar ) {
+        $collapsableItems.each(function(){
+            $(this).collapse('hide');
+        });
+    }
+});
+
 $(document).ready(function(){
     $(".table").hide();
 });
+
+/*function resize() {
+    $("#section1").css({'height':($(window).height())+'px'});
+    $("#section2").css({'height':($(window).height())+'px'});
+    $("#section3").css({'height':($(window).height())+'px'});
+    $("#section41").css({'height':($(window).height())+'px'});
+    $("#section42").css({'height':($(window).height())+'px'});
+}
+
+$(document).ready(function() {
+    resize();
+    $(window).bind('resize', resize);
+});*/
 
 $(document).ready(function(){
 
